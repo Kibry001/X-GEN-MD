449e877f376bbe5904d92f8345fb12f3700701df
Kibry001 <131856080+Kibry001@users.noreply.github.com>
2023-11-07 03:12:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -26,15 +26,15 @@ import moment from 'moment-timezone'
 /*==============USER-INFO==============*/
 global.owner = [
   ['8801853262586', 'ꜱʟᴀꜱʜᴇʀ ᴏꜰꜰɪᴄɪᴀʟ', true], 
-  ['put your number', 'your-name', true] 
+  ['8801853262586', 'Ericson', true] 
 ] 
 
-global.mods = ['put-your-number','8801853262586'] //Don't replace this,If you want to add someone add any dev
-global.prems = ['put-premium-user-number','']
-global.nomorbot = 'your-bot-number' //Change it to your own
-global.nomorown = 'owner-number' //Change it to your own
-global.namebot = 'bot-name' //change it to your own
-global.nameown = 'owner-name' // change it to your own
+global.mods = ['8801853262586','8801853262586'] //Don't replace this,If you want to add someone add any dev
+global.prems = ['8801853262586','']
+global.nomorbot = '8801853262586' //Change it to your own
+global.nomorown = '8801853262586' //Change it to your own
+global.namebot = 'XGEN' //change it to your own
+global.nameown = 'Ericson' // change it to your own
 
 //*============= ==============*/
 
@@ -49,8 +49,8 @@ global.org = 'org-hE49SehUQEPAGJAy20yr4Om9'   // Get you Org-Key from 'https://p
 
 /*============= TAIMU =============*/
 let wibh = moment.tz('Asia/Dhaka').format('HH')
-    let wibm = moment.tz('Asia/Dhaka').format('mm')
-    let wibs = moment.tz('Asia/Dhaka').format('ss')
+    let wibm = moment.tz('Africa/Nairobi').format('mm')
+    let wibs = moment.tz('Africa/Nairobi').format('ss')
     let wktuwib = `${wibh} H ${wibm} M ${wibs} S`
     
     let d = new Date(new Date + 3600000)
